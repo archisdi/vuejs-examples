921591de435851c6a3e370782c7e376bb4be9794
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-01-24 14:42:40 +0700

    finish section 1

--- 00-introduction/01-first-vue-app/index.js
+++ 00-introduction/01-first-vue-app/index.js
@@ -2,5 +2,10 @@ new Vue({
     el: '#app',
     data: {
         message: 'Hello World'
+    },
+    methods: {
+        change_message: function (event) {
+            this.message = event.target.value;
+        }
     }
 });
