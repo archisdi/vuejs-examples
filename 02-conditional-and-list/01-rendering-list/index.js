7902d8d08a890b96415349c85d4fe1ebfafe055b
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-01-29 11:58:08 +0700

    finish list and conditional

---
+++ 02-conditional-and-list/01-rendering-list/index.js
@@ -0,0 +1,36 @@
+new Vue({
+    el: '#app',
+    data: {
+        tags: [
+            "quis",
+            "do",
+            "aute",
+            "quis",
+            "cillum",
+            "labore",
+            "minim"
+        ],
+        persons: [
+            {
+                "age": 21,
+                "name": "Buckner Klein"
+            },
+            {
+                "age": 12,
+                "name": "Cleveland Kim"
+            },
+            {
+                "age": 24,
+                "name": "Shelley Hicks"
+            },
+            {
+                "age": 34,
+                "name": "Tyler King"
+            },
+            {
+                "age": 44,
+                "name": "Cindy Joyce"
+            }
+        ]
+    }
+});
