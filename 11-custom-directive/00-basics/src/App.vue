bd92e584bd229b91b858c8d566249d7744aa9b0d
Archie <archie.isdiningrat@bizzy.co.id>
2018-03-15 15:30:29 +0700

    finish custom directives

---
+++ 11-custom-directive/00-basics/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Built-in Directives</h1>
+                <p v-text="'some text'"></p>
+                <p v-html="'<strong>strong text</strong>'"></p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+                <p v-highlight.delayed="'red'">Color This</p>
+                <p v-local-highlight:background.delayed.blink="{mainColor:'green', secondColor:'red'}">Highlight This</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    localHighlight: {
+      bind(el, binding, vnode) {
+        let delay = 0;
+        if (binding.modifiers["delayed"]) delay = 3500;
+
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor === secondColor
+                ? (currentColor = mainColor)
+                : (currentColor = secondColor);
+              if (binding.arg === "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, 1000);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg === "background") {
+              el.style.backgroundColor = binding.value,mainColor;
+            } else {
+              el.style.color = binding.value.mainColor;
+            }
+          }, delay);
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ 11-custom-directive/01-practice/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <p v-on-double:mouseenter="mouseEnter" v-on-double:mouseleave="mouseLeave">Click me</p>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    onDouble: {
+      bind(el, binding) {
+        el.onclick = function() {
+          let type = binding.arg;
+          let action = binding.value;
+          el.addEventListener(type, action);
+        };
+      }
+    }
+  },
+  methods: {
+    clicked() {
+      alert("i was click");
+    },
+    mouseEnter() {
+      console.log("mouse enter");
+    },
+    mouseLeave() {
+      console.log("mouse leave");
+    },
+  }
+};
+</script>
+
+<style>
+
+</style>
